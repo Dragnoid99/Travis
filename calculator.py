bd9a422ee298ff600689fa08bee36de425edd609
Dragnoid99 <47084013+Dragnoid99@users.noreply.github.com>
2019-03-06 15:26:57 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,41 @@
+#The fuction that adds 2 numbers
+
+def add ( x , y ):
+    return x + y ;
+
+#The fuction that subtracts 2 numbers
+
+def subtract ( x , y ):
+    return x - y ;
+
+#The fuction that multiplies 2 numbers    
+
+def multiply ( x , y ):
+    return x * y ;
+
+#The fuction that divides 2 numbers    
+
+def divide ( x , y ):
+    return x / y ;
+
+#Taking inputs as the numbers
+
+x = int ( input ( "Enter first number: " ) ) ;
+y = int ( input ( "Enter second number: " ) ) ;
+
+#Taking input as the operation
+
+operation = input ( "Enter the operation: " ) ;
+
+#Checking which operation it is and printing corresponding result
+
+if ( operation == '+' ):
+    print ( "Answer: " , add ( x , y ) ) ;
+elif ( operation == '-' ):
+    print ( "Answer: " , subtract ( x , y ) ) ;
+elif ( operation == '*' ):
+    print ( "Answer: " , multiply ( x , y ) ) ;
+elif ( operation == '/' ):
+    print ( "Answer: " , divide ( x , y ) ) ;
+else :
+    print ( "Wrong operation!!!" ) ;
