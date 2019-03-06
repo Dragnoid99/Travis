36f70252aec1537746eddad2420cc52ee4326974
Dragnoid99 <47084013+Dragnoid99@users.noreply.github.com>
2019-03-07 00:12:44 +0530

    Update and rename calculator.py to functions.py

---
+++ functions.py
@@ -0,0 +1,20 @@
+#The fuction that adds 2 numbers
+
+def add ( x , y ) :
+    return x + y;
+
+#The fuction that subtracts 2 numbers
+
+def subtract ( x , y ) :
+
+    return x - y;
+
+#The fuction that multiplies 2 numbers    
+
+def multiply ( x , y ) :
+    return x * y;
+
+#The fuction that divides 2 numbers    
+
+def divide ( x , y ) :
+    return x / y;
