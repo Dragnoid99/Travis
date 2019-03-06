50857a1d170b8e2c07d822d5e67ab0ce1eab1075
Dragnoid99 <47084013+Dragnoid99@users.noreply.github.com>
2019-03-07 00:22:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ from functions import divide
 def test_check_add():
     assert add ( 5 , 2 ) == 7
     
-#def test_check_wrong_add():
-#    assert add ( 9 , 4 ) == 7
+def test_check_wrong_add():
+    assert add ( 9 , 4 ) == 7
 
 def test_check_subtract():
     assert subtract ( 5 , 2 ) == 3
 
-#def test_check_wrong_subtract():
-#    assert subtract ( 7 , 2 ) == 3
+def test_check_wrong_subtract():
+    assert subtract ( 7 , 2 ) == 3
 
 def test_check_multiply():
     assert multiply ( 5 , 2 ) == 10
