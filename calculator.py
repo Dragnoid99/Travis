08f2f5f4beff71702fad69eb107f29b234b19f70
Dragnoid99 <47084013+Dragnoid99@users.noreply.github.com>
2019-03-06 23:50:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,22 +1,22 @@
 #The fuction that adds 2 numbers
 
-def add (x,y):
-    return x + y ;
+def add(x,y):
+    return x+y;
 
 #The fuction that subtracts 2 numbers
 
-def subtract ( x , y ):
-    return x - y ;
+def subtract(x,y):
+    return x-y;
 
 #The fuction that multiplies 2 numbers    
 
-def multiply ( x , y ):
-    return x * y ;
+def multiply(x,y):
+    return x*y;
 
 #The fuction that divides 2 numbers    
 
-def divide ( x , y ):
-    return x / y ;
+def divide(x,y):
+    return x/y;
 
 #Taking inputs as the numbers
 
